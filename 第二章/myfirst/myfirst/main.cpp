515984ba5d02cf20c4780b1f79511a5ebcfac216
StudyBJQ <18342688660@163.com>
2023-04-28 10:01:52 +0800

    第二章

---
+++ 第二章/myfirst/myfirst/main.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+int myfirst();
+int carrots();
+int getinfo();
+int sq();
+int ourfunc();
+int convert();
+
+int main()
+{
+	/*myfirst();
+	carrots();
+	getinfo();
+	sq();
+	ourfunc();*/
+	convert();
+
+	return 0;
+}
